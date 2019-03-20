e761cf6e080d3784666b6bba7e3b0d8f0763a2b5
Daniel Crewdson <daniel.crewdson@dewsign.co.uk>
2019-03-20 10:59:36 +0000

    we are gettin there

---
+++ src/assets/resources.js
@@ -0,0 +1,85 @@
+const types = [
+    {
+        name: 'Normal',
+        colour: `#A8A77A`
+    },{
+        name: 'Fire',
+        colour: `#EE8130`
+    },{
+        name: 'Water',
+        colour: `#6390F0`
+    },{
+        name: 'Electric',
+        colour: `#F7D02C`
+    },{
+        name: 'Grass',
+        colour: `#7AC74C`
+    },{
+        name: 'Ice',
+        colour: `#96D9D6`
+    },{
+        name: 'Fighting',
+        colour: `#C22E28`
+    },{
+        name: 'Poison',
+        colour: `#A33EA1`
+    },{
+        name: 'Ground',
+        colour: `#E2BF65`
+    },{
+        name: 'Flying',
+        colour: `#A98FF3`
+    },{
+        name: 'Psychic',
+        colour: `#F95587`
+    },{
+        name: 'Bug',
+        colour: `#A6B91A`
+    },{
+        name: 'Rock',
+        colour: `#B6A136`
+    },{
+        name: 'Ghost',
+        colour: `#735797`
+    },{
+        name: 'Dragon',
+        colour: `#6F35FC`
+    },{
+        name: 'Dark',
+        colour: `#705746`
+    },{
+        name: 'Steel',
+        colour: `#D685AD`
+    },{
+        name: 'Fairy',
+        colour: `#D685AD`
+    },
+]
+
+const resources = {
+        types: types,
+}
+
+
+export default resources
+
+
+
+// Normal Type: A8A77A
+// Fire Type:  EE8130
+// Water Type:  6390F0
+// Electric Type:  F7D02C
+// Grass Type:  7AC74C
+// Ice Type:  96D9D6
+// Fighting Type:  C22E28
+// Poison Type:  A33EA1
+// Ground Type:  E2BF65
+// Flying Type:  A98FF3
+// Psychic Type:  F95587
+// Bug Type:  A6B91A
+// Rock Type:  B6A136
+// Ghost Type:  735797
+// Dragon Type:  6F35FC
+// Dark Type:  705746
+// Steel Type:  B7B7CE
+// Fairy Type:  D685AD
---
+++ src/components/Home.vue
@@ -0,0 +1,141 @@
+<template>
+    <div>
+        <h1 class="m-4">PokéSearch</h1>
+        <p class="m-2">Written by <span><a class="no-underline hover:underline" href="https://twitter.com/crumb1e_">@crumb1e</a></span></p>
+        <form class="w-full mx-auto max-w-sm">
+        <div class="flex items-center border-b border-b-2 border-teal py-2">
+            <input v-model="searchQuery" class="appearance-none bg-transparent border-none w-full text-grey-darker mr-3 py-1 px-2 leading-tight focus:outline-none" type="text" placeholder="Search for a Pokemon" aria-label="Full name">
+            <button @click="fetchPokemon(searchQuery)" class="flex-no-shrink bg-teal hover:bg-teal-dark border-teal hover:border-teal-dark text-sm border-4 text-white py-1 px-2 rounded" type="button">
+            Search
+            </button>
+        </div>
+        <div id="error" v-if="searchError">
+            <p class="mt-5 mb-3">Sorry, that Pokémon could not be found :(</p>
+            <p class="mt-3">Did you spell it's name correctly?</p>
+            <p>Did you mean: </p>
+            <ul class="list-reset pt-2 flex flex-wrap -mx-2 overflow-hidden">
+                <li v-for="pokemon in similarPoke" :key="pokemon.id" class="my-2 px-2 w-1/4 overflow-hidden sm:w-1/3 md:w-1/3">
+                    <div class="hover:underline hover:bg-grey-lightest cursor-pointer " @click="fetchPokemon(pokemon.name)">
+                        <img :src="pokemon.sprites.front_default" :alt="pokemon.name">
+                        <a  class="no-underline">{{ pokemon.name }}</a>
+                    </div>
+                </li>
+            </ul>
+
+        </div>
+        </form>
+
+        <div v-if="singleMon" class="mx-auto mt-4 bg-grey-lighter max-w-sm">
+            <img :src="singleMon.sprites.front_default" alt="">
+            <h2 class=" mb-2">{{ toUpperCase(singleMon.name) }}</h2>
+            <div class=" flex items-stretch">
+                <ul class="list-reset flex-1 text-left m-4">
+                    <p class="font-bold">Types</p>
+                    <li v-for="type in types" :key="type.slot">
+                        <p :class="typeColour(type.type.name)">{{ type.type.name }}</p>
+                    </li>
+                </ul>
+                <ul class="list-reset flex-1 text-left m-4">
+                    <p class="font-bold">Stats</p>
+                    <li v-for="stat in stats" :key="stat.id">
+                        <p>{{ stat.stat.name }}: {{ stat.base_stat }}</p>
+                    </li>
+                </ul>
+            </div>
+        </div>
+
+    </div>
+</template>
+
+<script>
+import axios from 'axios'
+import resources from '../assets/resources.js'
+
+export default {
+    name: 'Home',
+
+    data() {
+        return {
+            pokemon: [],
+            pokemonNames: [],
+            similarPoke: [],
+            singleMon: null,
+            types: [],
+            stats: [],
+            searchQuery: '',
+            searchError: false,
+            tempSimilar: null,
+            resources: resources,
+            errors: [],
+        }
+    },
+
+    computed: {
+
+    },
+
+    mounted() {
+        axios.get('http://pokeapi.co/api/v2/pokemon/?limit=1000')
+            .then(response => {
+                this.pokemon = response.data
+                response.data.results.forEach(element => {
+                    this.pokemonNames.push(element.name)                    
+                });
+            })
+            .catch(e => {
+                this.errors.push(e)
+            })
+    },
+
+    methods: {
+        fetchPokemon(value) {
+            axios.get(`http://pokeapi.co/api/v2/pokemon/${value}`)
+                .then(response => {
+                    this.singleMon = response.data
+                    this.types = this.singleMon.types
+                    this.stats = this.singleMon.stats
+                    this.clearErrors()
+
+                })
+                .catch(e => {
+                    this.clearErrors()
+                    this.singleMon = null
+                    this.searchError = true
+                    this.findSimilar(value)
+                    this.errors.push(e)
+                })
+        },
+
+        getSimilar(value) {
+            axios.get(`http://pokeapi.co/api/v2/pokemon/${value}`)
+                .then(response => {
+                    this.tempSimilar = response.data
+                    this.similarPoke.push(this.tempSimilar)
+                })
+        },
+
+        typeColour(name) {
+            return `bg-type-${name}`
+        },
+
+        toUpperCase(value) {
+            return value.charAt(0).toUpperCase() + value.slice(1)
+        },
+
+        findSimilar(value) {
+            this.pokemonNames.forEach(element => {
+                if (element.includes(value)) {
+                    this.getSimilar(element)
+                }
+            });
+        },
+
+        clearErrors() {
+            this.errors = []
+            this.searchError = false
+            this.similarNames = []
+        },
+    },
+}
+</script>
+
