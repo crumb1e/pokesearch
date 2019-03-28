d95d63a623a23f0c5c5ad0fcdd7836b42a56eef3
Daniel Crewdson <daniel.crewdson@dewsign.co.uk>
2019-03-28 14:01:54 +0000

    about component

---
+++ src/components/About.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h1 class="m-4">PokéSearch</h1>
+        <p class="m-2">Written by <span><a class="no-underline hover:underline" href="https://twitter.com/crumb1e_">@crumb1e</a></span></p>
+
+        <p class="m-2">
+            A simple Pokémon index.
+        </p>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'About'
+    }
+</script>
--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,16 @@
 import Home from './components/Home.vue'
+import About from './components/About.vue'
 
 export default [
     {
-        path: '/',
+        path: '/:pokemon?',
+        name: 'home',
         component: Home,
+        props: true,
+    },
+    {
+        path: '/about',
+        name: 'about',
+        component: About,
     }
 ]
